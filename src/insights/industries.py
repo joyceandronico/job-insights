ff617d705c3886d04b78cfe2510e9a504236e3fc
joyceandronicco <joyceandronico@msn.com>
2022-12-14 19:41:03 -0300

    feat: :construction: Implemente a função get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,17 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    # sourcery skip: for-append-to-extend, list-comprehension
+    jobs = read(path)
+    jobs_list = []
+    for job in jobs:
+        if job["industry"] != "":
+            jobs_list.append(job["industry"])
+    print(job["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return set(jobs_list)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
